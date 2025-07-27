865b98a7c9190e499338f0446addb32b8d2f0271
Mark Skelton <mdskelton99@gmail.com>
2025-07-26 22:08:47 -0500

    Ticker

--- index.js
+++ index.js
@@ -7,18 +7,44 @@ dotenv.config({ quiet: true })
 const cdp = new CdpClient()
 
 async function request(address, token) {
-  await cdp.evm.requestFaucet({
-    address,
-    network: "base-sepolia",
-    token,
-  })
+  try {
+    await cdp.evm.requestFaucet({
+      address,
+      network: "base-sepolia",
+      token,
+    })
+
+    return true
+  } catch (error) {
+    if (error.errorType === "faucet_limit_exceeded") {
+      return false
+    }
+
+    throw error
+  }
 }
 
-const tasks = [...Array(10).fill("usdc"), ...Array(30).fill("eth")]
+const USDC = 10
+const ETH = 30
 
-const bar = new ProgressBar("[:bar] :current/:total", { total: tasks.length })
+const bar = new ProgressBar("[:bar] :current/:total", {
+  total: USDC + ETH,
+})
 
-for (const token of tasks) {
-  await request(process.env.ADDRESS, token)
-  bar.tick()
+async function requestMultipleTimes(token, count) {
+  for (let i = 0; i < count; i++) {
+    const success = await request(process.env.ADDRESS, token)
+
+    if (!success) {
+      console.log(`${token.toUpperCase()} limit exceeded`)
+      bar.tick(count - i)
+      return
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    bar.tick()
+  }
 }
+
+await requestMultipleTimes("usdc", USDC)
+await requestMultipleTimes("eth", ETH)
